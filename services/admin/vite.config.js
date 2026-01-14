387b131422e616aab65d3383ef90d0ae4fa61c5f
Marcohau2O <Haupech2@gmail.com>
2026-01-13 19:31:48 -0500

    Cambios para Docker v1

--- services/admin/vite.config.js
+++ services/admin/vite.config.js
@@ -13,6 +13,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5000,
-    strictPort: true
+    strictPort: true,
+    watch: {
+      usePolling: true
+    }
   }
 })
--- services/web/vite.config.js
+++ services/web/vite.config.js
@@ -13,6 +13,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    strictPort: true
+    strictPort: true,
+    watch: {
+      usePolling: true
+    }
   }
 })
